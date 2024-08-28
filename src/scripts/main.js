108b7c5d540a09d08ea06f1930b4faaaddb8e502
VitorinoAlves <vitorino.17alexandre@gmail.com>
2024-08-28 15:57:30 -0300

    Creating countdown

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(() => {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteEvento = timeStampDoEvento-timeStampAtual;
+
+    const diasAteOEvento = Math.floor(distanciaAteEvento / (1000 * 60 * 60 * 24));
+    const horasAteOEvento = Math.floor((distanciaAteEvento % (1000 * 60 * 60 * 24))/(1000 * 60 * 60));
+    const minutosAteOEvento = Math.floor((distanciaAteEvento%(1000 * 60 * 60))/(1000 * 60));
+    const segundosAteOEvento = Math.floor((distanciaAteEvento%(1000 * 60))/1000);
+
+    document.getElementById('contador').innerHTML=`${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if(distanciaAteEvento<0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML='O evento já começou!';
+    }
+}, 1000);
